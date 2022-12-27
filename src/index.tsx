26a82f76cfb12214affa9443b99137bde86fc8bd
mmajudith <mmajudith2000@gmail.com>
2022-12-27 15:44:14 -0800

    refactor
    refactor index.ts code

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
 import { store } from './reduxToolKit/app/store';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
